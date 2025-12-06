4a36cc6b1f5001fbe3f2d1aa8a70683305d3a836
Antoine1206 <antoine.melanger@laposte.net>
2025-12-06 18:33:39 +0100

    Mise en place de l'environement - Ajout d'une SecurityConfig temporaire. - redimension pom.xml - Ajout configuration application.yaml

---
+++ src/main/java/com/mlm/backend/security/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.mlm.backend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        return new InMemoryUserDetailsManager(
+                User.withUsername("root")
+                        .password("{noop}root") // mot de passe root en clair
+                        .roles("USER")
+                        .build()
+        );
+    }
+}
